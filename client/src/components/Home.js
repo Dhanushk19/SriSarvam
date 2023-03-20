5554bf6812b8811fdd8c84e4d5566b64185de0eb
Dhanushk19 <95034453+Dhanushk19@users.noreply.github.com>
2023-03-20 23:13:10 +0530

    Home updated

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,7 +9,8 @@ import sunFil from "../assets/SunFil.jpg";
 import noprev from "../assets/noaddedprev.jpg";
 import adultfree from "../assets/adulterationfree.png";
 import natprod from "../assets/natprod.jpg";
-
+import woodp from "../assets/woodp.jpg";
+import logo1 from "../assets/logo1b.png";
 function Home() {
   return (
     <>
@@ -56,12 +57,59 @@ function Home() {
           />
         </Carousel.Item>
       </Carousel>
-
+      <div className="prodfull">
+      <h3 className="oprod">Our Products</h3>
+      <div class="row" id="oil">
+        <div class="col-sm-3" >
+          <a href="/products" className="anchor">
+          <div class="card1" >
+            <div class="card-body">
+              <img src={oil1} alt="Coconut oil" className="sun1"/>
+              <h5 class="card-title1" >Coconut Oil</h5>
+            </div>
+          </div>
+          </a>
+        </div>
+        <div class="col-sm-3">
+        <a href="/products" className="anchor">
+          <div class="card1">
+            <div class="card-body">
+            <img src={oil2} alt="Groundnut Oil" className="sun1"/>
+              <h5 class="card-title1">Groundnut Oil</h5>
+              
+            </div>
+          </div>
+          </a>
+        </div>
+        <div class="col-sm-3">
+        <a href="/products" className="anchor">
+          <div class="card1">
+            <div class="card-body">
+            <img src={oil3} alt="Gingelly Oil" className="sun1"/>
+              <h5 class="card-title1">Gingelly Oil</h5>
+              
+            </div>
+          </div>
+          </a>
+        </div>
+        <div class="col-sm-3">
+        <a href="/products" className="anchor">
+          <div class="card1">
+            <div class="card-body">
+            <img src={oil4} alt="Castor oil" className="sun1"/>
+              <h5 class="card-title1">Castor oil</h5>
+              
+            </div>
+          </div>
+          </a>
+        </div>
+      </div>
+      </div>
 
       <h4 className="Quote1">Quality in Every Drop</h4>
 
 
-      <div class="row">
+      <div class="row" id="info" >
         <div class="col-sm-3">
           <div class="card">
             <div class="card-body">
@@ -98,6 +146,36 @@ function Home() {
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="adv">
+        <h4 className="why">Why use Wood Pressed Oil ?</h4>
+          <div className="row">
+            <div className="col-sm-6">
+              <img src={woodp} alt="woodpress" className="woo"></img>
+            </div>
+            <div className="col-sm-6">
+              <p>Wood pressed oil, also known as cold-pressed oil or chekku oil, is extracted from seeds or nuts using traditional wooden presses.Wood pressed oils are often considered to be more nutrient-rich than oils extracted using modern methods. This is because the wooden press method allows for the retention of natural nutrients and antioxidants.Wood pressed oils are extracted without the use of any chemicals, solvents or heat. This means that the oil is free from any harmful residues that could be left behind from chemical extraction processes.</p>
+            </div>
+          </div>
+      </div>
+    
+      <div className="compdetails">
+          <img src={logo1} alt="compname" className="comname"></img>
+           <div className="row">
+            <div className="col-sm-4">
+            <h5>Any Doubts ? :</h5>
+           <h6 className="doubt">&nbsp;<i class="fa-solid fa-phone"></i>&nbsp;9788718180</h6>
+            </div>
+            <div className="col-sm-4">
+            <h5>Address :</h5>
+           <h6 className="address">&nbsp;&nbsp;<i class="fa-solid fa-location-dot"></i>&nbsp;&nbsp;Sri Vaari Electrical<br></br> Building , Karur main road ,<br></br> Kalipalayam , Dharapuram<br></br> block , Tiruppur District,<br></br> Tamil nadu - 638661 .</h6>
+            </div>
+            <div className="col-sm-4">
+            <h5>Email :</h5>
+           <h6 className="email">&nbsp;&nbsp;<i class="fa-solid fa-envelope"></i>&nbsp;&nbsp;sample@gmail.com</h6>
+            </div>
+           </div>
       </div>
       
     </>
