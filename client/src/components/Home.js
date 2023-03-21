aa7e7e60739f3c1b4da9f64eb6d8b1da7d19c056
Dhanushk19 <95034453+Dhanushk19@users.noreply.github.com>
2023-03-21 15:51:47 +0530

    home page updated

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,11 +11,20 @@ import adultfree from "../assets/adulterationfree.png";
 import natprod from "../assets/natprod.jpg";
 import woodp from "../assets/woodp.jpg";
 import logo1 from "../assets/logo1b.png";
+import Typer from "./typical";
+import ScrollToTop from "../components/top.js";
 function Home() {
   return (
     <>
       <h4 className="home"><i class="fa fa-home" aria-hidden="true">&nbsp;</i>Home</h4>
-      <h2 className="compname">Sri Sarvam oils</h2>
+      <h6 className="compname"><Typer heading="Sri Sarvam Oils : "
+        dataText={[
+          "Nature's goodness in every drop.",
+          "Pure and unadulterated oil", " straight from the source.",
+          "The way nature intended it.",
+          "Healthy living begins with pure, wood-pressed oil.",
+          "Traditionally made, naturally pure."
+        ]} /></h6>
       <Carousel className="caromod">
         <Carousel.Item>
           <img
@@ -58,52 +67,52 @@ function Home() {
         </Carousel.Item>
       </Carousel>
       <div className="prodfull">
-      <h3 className="oprod">Our Products</h3>
-      <div class="row" id="oil">
-        <div class="col-sm-3" >
-          <a href="/products" className="anchor">
-          <div class="card1" >
-            <div class="card-body">
-              <img src={oil1} alt="Coconut oil" className="sun1"/>
-              <h5 class="card-title1" >Coconut Oil</h5>
-            </div>
+        <h3 className="oprod">Our Products</h3>
+        <div class="row" id="oil">
+          <div class="col-sm-3" >
+            <a href="/products" className="anchor">
+              <div class="card1" >
+                <div class="card-body">
+                  <img src={oil1} alt="Coconut oil" className="sun1" />
+                  <h5 class="card-title1" >Coconut Oil</h5>
+                </div>
+              </div>
+            </a>
           </div>
-          </a>
-        </div>
-        <div class="col-sm-3">
-        <a href="/products" className="anchor">
-          <div class="card1">
-            <div class="card-body">
-            <img src={oil2} alt="Groundnut Oil" className="sun1"/>
-              <h5 class="card-title1">Groundnut Oil</h5>
-              
-            </div>
+          <div class="col-sm-3">
+            <a href="/products" className="anchor">
+              <div class="card1">
+                <div class="card-body">
+                  <img src={oil2} alt="Groundnut Oil" className="sun1" />
+                  <h5 class="card-title1">Groundnut Oil</h5>
+
+                </div>
+              </div>
+            </a>
           </div>
-          </a>
-        </div>
-        <div class="col-sm-3">
-        <a href="/products" className="anchor">
-          <div class="card1">
-            <div class="card-body">
-            <img src={oil3} alt="Gingelly Oil" className="sun1"/>
-              <h5 class="card-title1">Gingelly Oil</h5>
-              
-            </div>
+          <div class="col-sm-3">
+            <a href="/products" className="anchor">
+              <div class="card1">
+                <div class="card-body">
+                  <img src={oil3} alt="Gingelly Oil" className="sun1" />
+                  <h5 class="card-title1">Gingelly Oil</h5>
+
+                </div>
+              </div>
+            </a>
           </div>
-          </a>
-        </div>
-        <div class="col-sm-3">
-        <a href="/products" className="anchor">
-          <div class="card1">
-            <div class="card-body">
-            <img src={oil4} alt="Castor oil" className="sun1"/>
-              <h5 class="card-title1">Castor oil</h5>
-              
-            </div>
+          <div class="col-sm-3">
+            <a href="/products" className="anchor">
+              <div class="card1">
+                <div class="card-body">
+                  <img src={oil4} alt="Castor oil" className="sun1" />
+                  <h5 class="card-title1">Castor oil</h5>
+
+                </div>
+              </div>
+            </a>
           </div>
-          </a>
         </div>
-      </div>
       </div>
 
       <h4 className="Quote1">Quality in Every Drop</h4>
@@ -113,36 +122,36 @@ function Home() {
         <div class="col-sm-3">
           <div class="card">
             <div class="card-body">
-              <img src={sunFil} alt="SunLight Filteration" className="sun"/>
+              <img src={sunFil} alt="SunLight Filteration" className="sun" />
               <h5 class="card-title" >Sunlight Filteration</h5>
-              
+
             </div>
           </div>
         </div>
         <div class="col-sm-3">
           <div class="card">
             <div class="card-body">
-            <img src={noprev} alt="No Preservatives" className="sun"/>
+              <img src={noprev} alt="No Preservatives" className="sun" />
               <h5 class="card-title">No Preservatives</h5>
-              
+
             </div>
           </div>
         </div>
         <div class="col-sm-3">
           <div class="card">
             <div class="card-body">
-            <img src={adultfree} alt="Adulteration Free" className="sun"/>
+              <img src={adultfree} alt="Adulteration Free" className="sun" />
               <h5 class="card-title">Adulteration Free</h5>
-              
+
             </div>
           </div>
         </div>
         <div class="col-sm-3">
           <div class="card">
             <div class="card-body">
-            <img src={natprod} alt="Natural Product" className="sun"/>
+              <img src={natprod} alt="Natural Product" className="sun" />
               <h5 class="card-title">No Chemicals</h5>
-              
+
             </div>
           </div>
         </div>
@@ -150,34 +159,34 @@ function Home() {
 
       <div className="adv">
         <h4 className="why">Why use Wood Pressed Oil ?</h4>
-          <div className="row">
-            <div className="col-sm-6">
-              <img src={woodp} alt="woodpress" className="woo"></img>
-            </div>
-            <div className="col-sm-6">
-              <p>Wood pressed oil, also known as cold-pressed oil or chekku oil, is extracted from seeds or nuts using traditional wooden presses.Wood pressed oils are often considered to be more nutrient-rich than oils extracted using modern methods. This is because the wooden press method allows for the retention of natural nutrients and antioxidants.Wood pressed oils are extracted without the use of any chemicals, solvents or heat. This means that the oil is free from any harmful residues that could be left behind from chemical extraction processes.</p>
-            </div>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={woodp} alt="woodpress" className="woo"></img>
           </div>
+          <div className="col-sm-6">
+            <p>Wood pressed oil, also known as cold-pressed oil or chekku oil, is extracted from seeds or nuts using traditional wooden presses.Wood pressed oils are often considered to be more nutrient-rich than oils extracted using modern methods. This is because the wooden press method allows for the retention of natural nutrients and antioxidants.Wood pressed oils are extracted without the use of any chemicals, solvents or heat. This means that the oil is free from any harmful residues that could be left behind from chemical extraction processes.</p>
+          </div>
+        </div>
       </div>
-    
+
       <div className="compdetails">
-          <img src={logo1} alt="compname" className="comname"></img>
-           <div className="row">
-            <div className="col-sm-4">
-            <h5>Any Doubts ? :</h5>
-           <h6 className="doubt">&nbsp;<i class="fa-solid fa-phone"></i>&nbsp;9788718180</h6>
-            </div>
-            <div className="col-sm-4">
-            <h5>Address :</h5>
-           <h6 className="address">&nbsp;&nbsp;<i class="fa-solid fa-location-dot"></i>&nbsp;&nbsp;Sri Vaari Electrical<br></br> Building , Karur main road ,<br></br> Kalipalayam , Dharapuram<br></br> block , Tiruppur District,<br></br> Tamil nadu - 638661 .</h6>
-            </div>
-            <div className="col-sm-4">
-            <h5>Email :</h5>
-           <h6 className="email">&nbsp;&nbsp;<i class="fa-solid fa-envelope"></i>&nbsp;&nbsp;sample@gmail.com</h6>
-            </div>
-           </div>
+        <img src={logo1} alt="compname" className="comname"></img>
+        <div className="row">
+          <div className="col-sm-4">
+            <h5 className="bottom"> Any Doubts ? :</h5>
+            <h6 className="doubt"><i class="fa-solid fa-phone"></i>&nbsp;9788718180</h6>
+          </div>
+          <div className="col-sm-5">
+            <h5 className="bottom">Address :</h5>
+            <h6 className="address"><i class="fa-solid fa-location-dot"></i>&nbsp;&nbsp;Sri Vaari Electrical Building ,<br></br> Karur main road ,<br></br> Kalipalayam ,<br></br> Tiruppur District,<br></br> Tamil nadu - 638661 .</h6>
+          </div>
+          <div className="col-sm-3">
+            <h5 className="bottom">Email :</h5>
+            <h6 className="email"><i class="fa-solid fa-envelope"></i>&nbsp;&nbsp;sample@gmail.com</h6>
+          </div>
+        </div>
       </div>
-      
+        <ScrollToTop/>
     </>
   )
 }
