75d6a65c0ce8f1a0fc11fa9c20d1f94f955fb2a9
Karthikeyan S <karthiselvam8292@gmail.com>
2023-03-28 15:55:04 +0530

    about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,18 +1,18 @@
 import "../css/About.css";
-import woodimg from '../assets/woodimg.jpg';
-import aliveoil from '../assets/transform.jpeg';
-import coconut from '../assets/coconut.jpeg';
-import castor from '../assets/images2.jpeg';
-import ginger from '../assets/Ginger-oil.jpg';
-import olives from '../assets/oliveoils.jpg'
+import woodimg from "../assets/woodimg.jpg";
+import aliveoil from "../assets/transform.jpeg";
+import coconut from "../assets/coconut.jpeg";
+import castor from "../assets/images2.jpeg";
+import ginger from "../assets/Ginger-oil.jpg";
+import olives from "../assets/oliveoils.jpg";
 
-
-function About()
-{
-    return(
-        <>
-        <h4 className="about"><i class="fa-solid fa-circle-info"></i>&nbsp; About</h4>
-        <div class="container-fluid">
+function About() {
+  return (
+    <>
+      <h4 className="about">
+        <i class="fa-solid fa-circle-info"></i>&nbsp; About
+      </h4>
+      {/* <div class="container-fluid">
             
             <div className="image">
                 <img className="imglogo" src={woodimg}/>
@@ -34,61 +34,77 @@ function About()
         <div className="images">
             <img className="imglogos" src={aliveoil} />
         </div>
-        </div>
-        <div className="product">
-            <h2>OUR PRODUCT</h2>
-        </div>
-        <div class="responsive">
-        <div class="gallery">
-            <a target="_blank" href="img_5terre.jpg">
-            <img className="imglogos" src={coconut}/>
+        </div> */}
 
-            </a>
-            <div class="desc">Coconut Oil</div>
-        </div>
-        </div>
+      
+
+      <section class="section mt-5">
         
-        <div class="responsive">
+          <div class="row">
+            <div class="col-lg-6">
+              <div className="image">
+                <img className="imglogos" src={aliveoil} />
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-6  d-flex align-items-center mt-4 mt-md-0 pt-3 ">
+              <div>
+                <p>Align image and text in same line in bootstrap</p>
+              </div>
+            </div>
+          </div>
+       
+      </section>
+
+      <div className="product">
+        <h2>OUR PRODUCT</h2>
+      </div>
+      <div class="responsive">
         <div class="gallery">
-            <a target="_blank" href="img_forest.jpg">
-            <img className="imglogos" src={castor}/>
+          <a target="_blank" href="img_5terre.jpg">
+            <img className="imglogos" src={coconut} />
+          </a>
+          <div class="desc">Coconut Oil</div>
+        </div>
+      </div>
 
-            </a>
-            <div class="desc">Castor Oil</div>
-        </div>
-        </div>
-        <div class="responsive">
+      <div class="responsive">
         <div class="gallery">
-            <a target="_blank" href="img_lights.jpg">
-            <img className="imglogos" src={ginger}/>
-
-            </a>
-            <div class="desc">Ginger Oil</div>
+          <a target="_blank" href="img_forest.jpg">
+            <img className="imglogos" src={castor} />
+          </a>
+          <div class="desc">Castor Oil</div>
         </div>
-        </div>
-        <div class="responsive">
+      </div>
+      <div class="responsive">
         <div class="gallery">
-            <a target="_blank" href="img_mountains.jpg">
-            <img className="imglogos" src={olives}/>
-
-            </a>
-            <div class="desc">Olive Oil</div>
+          <a target="_blank" href="img_lights.jpg">
+            <img className="imglogos" src={ginger} />
+          </a>
+          <div class="desc">Ginger Oil</div>
         </div>
+      </div>
+      <div class="responsive">
+        <div class="gallery">
+          <a target="_blank" href="img_mountains.jpg">
+            <img className="imglogos" src={olives} />
+          </a>
+          <div class="desc">Olive Oil</div>
         </div>
+      </div>
 
-        <div className="foot">
-            <h2>Thank you for choosing our Cooking Oil Company for all your cooking needs. <br/>
-                We look forward to serving you! :) :)</h2>
-        </div>
-        <div className="footer">
-        {/* <i class='fas fa-thumbs-up' style='font-size:36px'></i> */}
-        <i class='fas fa-thumbs-up'></i>
-        </div>        
-        </>
-
-        
-
-    )
+      <div className="foot">
+        <h2>
+          Thank you for choosing our Cooking Oil Company for all your cooking
+          needs. <br />
+          We look forward to serving you! :) :)
+        </h2>
+      </div>
+      <div className="footer">
+        <i class="fas fa-thumbs-up"></i>
+      </div>
+      
+    </>
+  );
 }
 
 export default About;
