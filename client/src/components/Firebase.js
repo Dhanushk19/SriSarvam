43d7ff0b74300d9bf3af2d462373b332b6d0cdd3
Dhanushk19 <95034453+Dhanushk19@users.noreply.github.com>
2023-04-05 09:11:49 +0530

    login page

---
+++ client/src/components/Firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQN5wJdMU_AA5NLfBfVm--fyHwnffVn-g",
+  authDomain: "srisarvam-9435d.firebaseapp.com",
+  projectId: "srisarvam-9435d",
+  storageBucket: "srisarvam-9435d.appspot.com",
+  messagingSenderId: "345927404161",
+  appId: "1:345927404161:web:36242ee528d6c431dc5b80"
+};
+
+
+const app = initializeApp(firebaseConfig);
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,41 @@
+import React from "react";
+import "../css/Login.css";
+import { Form, Alert } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 function Login()
 {
     return(
-        <h3>Login</h3>
+        <>
+        <h5 className="home">Login</h5>
+        <div className="fullform">
+        <h4 className="admin">Hello Admin !</h4>
+        <br></br>
+        <Form >
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              type="email"
+              placeholder="Email address"
+              
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+             
+            />
+          </Form.Group>
+
+          <div className="d-grid gap-2">
+            <Button variant="success" type="Submit">
+             <div  >Login</div> 
+            </Button>
+          </div>
+        </Form>
+        </div>
+    </>
+        
     )
 }
 
